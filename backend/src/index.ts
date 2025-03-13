9bf05f842d1d4b2bdcc429b5eac0922c628814d9
Vladimir Noutbook <vovirus2@inbox.ru>
2025-03-13 12:20:39 +0300

    добавили trpc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hello woerl')
+import express from 'express'
+
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { trpcRoute } from './trps'
+
+const expressApp = express()
+
+expressApp.get('/ping', (req, res) => {
+  res.send('pong dd')
+})
+expressApp.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: trpcRoute,
+  })
+)
+
+expressApp.listen(3000, () => {
+  console.info('Listining at http://localhost:3000/ ')
+})
