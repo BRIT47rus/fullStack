e8d528e8779dc64d5e3d1e3d34936847000e69a0
Vladimir Noutbook <vovirus2@inbox.ru>
2025-03-14 10:04:48 +0300

    try

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 
 import * as trpcExpress from '@trpc/server/adapters/express';
-import { trpcRoute } from './trps'
+import { trpcRoute } from './trpc'
 
 const expressApp = express()
 
